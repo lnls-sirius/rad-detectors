6788816fff515d91cf5fe5a929e4f556b6a37ce9
RafaelLyra8 <rafaellyra2010@hotmail.com>
2023-02-14 07:58:03 -0300

    Default Server Modification

--- front_end/src/data-access/EPICS/Network.js
+++ front_end/src/data-access/EPICS/Network.js
@@ -4,11 +4,12 @@ class Network {
             Network.instance = this;
         }
         this.secure = window.location.protocol === 'https:';
-        this.epics2webHost = '10.0.38.42';
+        this.epics2webHost = '10.0.38.46';
         this.epics2webLocation = '/epics2web';
         this.epics2webGet = this.secure ? 'https://' : 'http://' + this.epics2webHost + this.epics2webLocation + '/caget';
         this.epics2webWs =
             this.secure ? 'wss://' : 'ws://' + this.epics2webHost + this.epics2webLocation + '/monitor';
+            console.log(this.epics2webWs)
         this.epics2webOptions = {
             url: this.epics2webWs,
             autoOpen: true,
--- front_end/src/data-access/archiver/archiver.ts
+++ front_end/src/data-access/archiver/archiver.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ArchiverData, ArchiverDataPoint, DataAccess } from '../../assets/interfaces/access-data';
 
 export const ipRegExp = /https?\/((?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])))\//;
-export const defaultHost = "10.0.38.42";
+export const defaultHost = "10.0.38.46";
 
 export class ArchiverDataAccess implements DataAccess{
 
@@ -91,7 +91,6 @@ export class ArchiverDataAccess implements DataAccess{
     }
 
     if (window.location.host === "localhost:3000"
-        || window.location.host === "10.0.105.5:3000"
         || window.location.host === "10.0.105.5:3000") {
       this.host = defaultHost;
       console.log(`DEBUG SERVER. Setting host to ${this.host}`);
