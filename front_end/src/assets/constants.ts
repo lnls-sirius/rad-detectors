ad3a95833af728e72963da69018722587eb6d5e0
Rafael Lyra <rafael.lyra@lnls.br>
2023-04-18 09:53:16 -0300

    Version format fix

--- front_end/src/assets/constants.ts
+++ front_end/src/assets/constants.ts
@@ -2,7 +2,7 @@ import GitInfo from 'react-git-info/macro';
 import { DDictStr, DictEnumStr, DictNum, DictStr } from "./interfaces/patterns"
 
 const git_version = GitInfo()
-const version: string = "v"+git_version.tags + "--" + git_version.commit.date;
+const version: string = "v1.0.0."+git_version.commit.shortHash + " / " + git_version.commit.date;
 
 // Integrated dose limits parameters
 const led_limits: DictNum = {
