7b1b9b63708c7392427bcdb2bdcdd2d065902d81
Rafael Lyra <rafael.lyra@lnls.br>
2023-09-21 16:02:55 -0300

    Update Ldap-api communication

--- front_end/src/data-access/Ldap_auth/index.ts
+++ front_end/src/data-access/Ldap_auth/index.ts
@@ -1,27 +1,30 @@
 import axios from "axios";
 import { JSEncrypt } from "jsencrypt";
-import { DictStr } from "../../assets/interfaces/patterns";
+import { DictStr, DictEnumStr } from "../../assets/interfaces/patterns";
 import { public_key, public_key_api, private_key } from "../../assets/keys";
 import { Buffer } from "buffer";
 
-function encrypt_request(login_data: DictStr, public_key_api: string): DictStr {
-  let encrypted_data: DictStr = {};
+function encrypt_request(login_data: DictStr, public_key_api: string): DictEnumStr {
+  let encrypted_data: DictEnumStr = {};
   let cipher = new JSEncrypt({
     default_key_size: "2048"
   });
   cipher.setPublicKey(public_key_api);
   Object.entries(login_data).map(([key, value]: [string, string])=>{
-    let value_enc: string = "";
-    if (key != "public_key"){
-      const encrypted_result = cipher.encrypt(value);
+    let value_enc: string[] = [];
+    let iterations: number = Math.ceil(value.length/150);
+
+    for(let it=0; it<iterations; it++){
+      const it_pos = 150 * it;
+      const substr: string = value.substring(it_pos, it_pos+150);
+      const encrypted_result = cipher.encrypt(substr);
       if(encrypted_result){
-        value_enc = encrypted_result;
+        value_enc.push(encrypted_result);
       }
-    }else{
-      value_enc = value;
     }
     encrypted_data[key] = value_enc;
   })
+
   return encrypted_data
 }
 
@@ -31,20 +34,23 @@ function decrypt_response(response_data: string): DictStr {
     default_key_size: "2048"
   });
   cipher.setPrivateKey(private_key);
-  Object.entries(response_data).map(([key, value]: any)=>{
+  Object.entries(response_data).map(([key, value_list]: any)=>{
     let value_enc: string = "";
-    const new_val = Buffer.from(value).toString('latin1');
-    const encrypted_result = cipher.decrypt(new_val);
-    if(encrypted_result){
-      value_enc = encrypted_result;
-    }
-    encrypted_data[key] = value_enc;
+    encrypted_data[key] = "";
+    value_list.map((value: string) => {
+      const new_val = Buffer.from(value).toString('latin1');
+      const encrypted_result = cipher.decrypt(new_val);
+      if(encrypted_result){
+        value_enc = encrypted_result;
+      }
+      encrypted_data[key] += value_enc;
+    })
   })
   return encrypted_data
 }
 
 async function login_ldap(username: string, password: string, group: string): Promise<boolean> {
-  const jsonurl:string = `${window.location.protocol}//ldap-auth-api.lnls.br/group`;
+  const jsonurl:string = `${window.location.protocol}//127.0.0.1:5000/group`;
   const login_data: DictStr = {
     email: username,
     password: password,
@@ -52,7 +58,7 @@ async function login_ldap(username: string, password: string, group: string): Pr
     public_key: public_key
   }
 
-  const encrypted: DictStr = encrypt_request(login_data, public_key_api)
+  const encrypted: DictEnumStr = encrypt_request(login_data, public_key_api)
   const encrypt_string: string = JSON.stringify(encrypted)
   return await axios
     .post(jsonurl, {
